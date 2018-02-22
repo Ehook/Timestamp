dff0b758b752ee466fc2f2a07033bbf0aa9af2aa
ehook <ehook@launchthat.com>
2018-02-22 12:17:43 -0500

    Incoming!

--- public/Urlshortner.js
+++ public/Urlshortner.js
@@ -6,42 +6,48 @@ var MongoClient = require('mongodb').MongoClient;
 var app = express();
 app.use(bodyParser.json());
 app.use(cors());
-//app.use(express.static(__dirname + '/public'));
 
-//var url = '/new/:url(*)';
 var appurl = 'localhost:8000/'
-var urlrequest = 0;
 
 MongoClient.connect('mongodb://localhost:27017/urlshort', function(err, db){
   app.get('/new/:url(*)' ,function(req, res){
     var initurl = req.params.url
-    urlrequest++
-    //var newurllength = new Array(initurl);
-    var newurl = appurl + (urlrequest * 2 + 1000);
-    //var result = newurl.link(initurl)
+    var uniqueID = new Date().getTime();
+    uniqueID = uniqueID.toString();
+    uniqueID = uniqueID.slice(0, -2);
+    var newurl = appurl + uniqueID;
     db.collection('urlshort').insert({
         "longUrl": initurl,
         "shortUrl": newurl,
-        "uniqueID": urlrequest
+        "uniqueID": uniqueID
     }, function(error, data){
       if(error){
         console.log("There was a problem collecting data")
       }
-      console.log(data)
+      var long = data.ops[0].longUrl
+      var shortened = data.ops[0].shortUrl
+      var obj = {"original_url": long, "short_url": shortened}
+      res.send(obj)
     })
-    /*if(console.error()){
-      throw err;
-    }
-    else{
-      response.json({'NewURL': result, 'OldURL': initurl})
-      console.log('Created New Link! ' + newurl)
-    }*/
   })
-})
-app.get('/:newurl', function(req, res){
-  var shortenedurl = req.params.newurl;
-  if(shortenedurl.indexOf("http") == -1){shortenedurl = "http://"+shortenedurl}
-  req.redirect(shortenedurl);
+  app.get('/:uniqueID', function(req, res){
+    var uniqueID = req.params.uniqueID;
+
+    db.collection('urlshort').find({
+      "uniqueID": uniqueID
+    }).toArray(function(err, data){
+      if(err){
+        return console.log("There was an error finding the shortened url")
+      }
+      if(data.length == 0){
+        return console.log("There is no shorten url matches this one in the database")
+      }
+      var l = data[0].longUrl
+      if(l.indexOf("http") == -1){l = "http://"+ l}
+      res.redirect(l);
+    })
+
+  })
 })
 
 app.listen(8000, function(){
